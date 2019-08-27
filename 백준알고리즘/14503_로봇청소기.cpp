8ae2a8b62f1afea124056fb677c1b2a4256a2051
hobbang2 <cattie@pukyong.ac.kr>
2019-08-27 17:33:22 +0900

    sovle 14503_로봇청소기.cpp

---
+++ 백준알고리즘/14503_로봇청소기.cpp
@@ -0,0 +1,114 @@
+
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+enum {PATH,WALL,DONE=7};
+
+int N,M;
+int map[51][51] = {0,};
+int dir_x[4] = {0,1,0,-1};
+int dir_y[4] = {-1,0,1,0};
+
+typedef struct Coord{
+    int y;
+    int x;
+    int dir;
+    Coord(int _y, int _x, int _dir){
+        y = _y; x= _x; dir = _dir;
+    };
+}Coord;
+
+bool canRobotGo(int y,int x){
+    if(y < 0 || y >= N || x < 0 || x >= M){
+        return false;
+    }
+    if(map[y][x] == PATH){
+        return true;
+    }
+    return false;
+}
+
+int cntCleanArea(){
+    int cnt = 0;
+    printf("\n\n");
+    for(int y= 0 ; y< N; y++){
+        for(int x=  0; x <M; x++){
+            printf("%d ",map[y][x]);
+            if(map[y][x] == DONE){
+                cnt++;
+            }
+        }
+        printf("\n");
+    }
+    return cnt;
+}
+
+void changeDir(Coord & curRobot){
+    curRobot.dir = (curRobot.dir - 1 < 0) ? (curRobot.dir + 3) : (curRobot.dir - 1);
+}
+
+void StartCleaning(Coord & curRobot){
+    for(;;){
+        map[curRobot.y][curRobot.x] = DONE;
+        bool cleaning = false;
+        int initDir = curRobot.dir;
+        int cnt = 0;
+
+        for(; cnt < 4; cnt ++){
+            int next_dir = (curRobot.dir - 1 < 0) ? (curRobot.dir + 3) : (curRobot.dir - 1);
+
+            int next_y = curRobot.y + dir_y[next_dir];
+            int next_x = curRobot.x + dir_x[next_dir];
+
+            if(canRobotGo(next_y,next_x) == true){
+                curRobot.y = next_y;
+                curRobot.x = next_x;
+                curRobot.dir = next_dir;
+                cleaning = true;
+                break;
+            }
+            else{
+                if(next_dir == initDir){
+                    break;
+                }
+                else{
+                    curRobot.dir = next_dir;
+                }
+            }
+        }
+
+        if(cleaning == false){
+            int next_y = curRobot.y - dir_y[initDir];
+            int next_x = curRobot.x - dir_x[initDir];
+            if(map[next_y][next_x]==WALL){
+                break;
+            }
+            else{
+                curRobot.y = next_y;
+                curRobot.x = next_x;
+                curRobot.dir = initDir;
+            }
+        }
+        
+    }
+
+}
+
+int main(){
+    Coord robot(0,0,0);
+    scanf("%d %d",&N,&M);
+
+    scanf("%d %d %d",&robot.y,&robot.x, &robot.dir);
+    for(int y = 0; y< N; y++){
+        for(int x =0; x < M ; x++){
+            scanf("%d",&map[y][x]);
+        }
+    }
+    
+    StartCleaning(robot);
+    int ans = cntCleanArea();
+    printf("%d\n",ans);
+    return 0;
+}
---
+++ 알고리즘풀이/문제집/README.md
@@ -0,0 +1,2 @@
+## 역량테스트 대비 문제 풀이 저장소 
+[삼성 SW 역량 테스트 기출 문제](https://www.acmicpc.net/workbook/view/1152)
