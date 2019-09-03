d604240b0d5c20d1992ada73c822a0d2b159a505
hobbang2 <cattie@pukyong.ac.kr>
2019-09-03 17:34:25 +0900

    fail_15685_드래곤커브_좌표계 생각하기 실패 ㅠㅠㅠㅠㅠㅠ

--- 백준알고리즘/15685_드래곤커브.cpp
+++ 백준알고리즘/15685_드래곤커브.cpp
@@ -6,71 +6,73 @@ using namespace std;
 
 int N = 0;
 int map[101][101];
-int dir_x[4] = {1,0,-1,0};
-int dir_y[4] = {0,-1,0,1};
+int dir_x[4] = { 1,0,-1,0 };
+int dir_y[4] = { 0,-1,0,1 };
 
-typedef struct Curve{
-    int y,x;
-    int d;
-    int gene;
+typedef struct Curve {
+	int y, x;
+	int d;
+	int gene;
 }Curve;
 
 vector <Curve> dragonCurve;
 
-void initVector(vector <Curve> & targetVec){
-    vector <Curve> emptyVec;
-    targetVec.swap(emptyVec);
-    return;
+void initVector(vector <Curve> & targetVec) {
+	vector <Curve> emptyVec;
+	targetVec.swap(emptyVec);
+	return;
 }
 
-void gameStart(const Curve & initCurve){
-    dragonCurve.push_back(initCurve);
+void gameStart(const Curve & initCurve) {
+	dragonCurve.push_back(initCurve);
 
-    for(int cnt = 1; cnt <= initCurve.gene; cnt++){
-        int pointNum = (int)dragonCurve.size();
-        Curve lastCurve = dragonCurve.back();
+	for (int cnt = 1; cnt <= initCurve.gene; cnt++) {
+		int pointNum = (int)dragonCurve.size();
+		Curve lastCurve = dragonCurve.back();
 
-        for(int num = 0; num < pointNum; num++){
-            Curve curCurve = dragonCurve[num];
-            map[curCurve.y][curCurve.x] = 1;
-            Curve nextCurve = {-102,-102,curCurve.d,curCurve.gene};
-            if(cnt == 1){
-                nextCurve.y = curCurve.y + dir_x[curCurve.d];
-                nextCurve.x = curCurve.x + dir_y[curCurve.d];
-            }
-            else{
-                nextCurve.y=  -curCurve.x + lastCurve.y;
-                nextCurve.x = curCurve.y+ lastCurve.x;
-            }
-            if(map[nextCurve.y][nextCurve.x] == 0){
-                dragonCurve.push_back(nextCurve);
-            }
-            
-        }
-    }
+		for (int num = 0; num < pointNum; num++) {
+			Curve curCurve = dragonCurve[num];
+			map[curCurve.y][curCurve.x] = cnt;
+			Curve nextCurve = {0,};
+			if (cnt == 1) {
+				nextCurve.y = curCurve.y + dir_y[curCurve.d];
+				nextCurve.x = curCurve.x + dir_x[curCurve.d];
+			}
+			else {
+				nextCurve.y = (100 - curCurve.x) + lastCurve.y - 50;
+				nextCurve.x = curCurve.y + lastCurve.x - 50;
+			}
+			dragonCurve.push_back(nextCurve);
+		}
+	}
 }
 
 
-int main(){
-    scanf("%d",&N);
-    
-    for(int n = 1 ; n  <= N; n++){
-        Curve curCurve = {-102,-102,-1,-1};
-        scanf("%d %d %d %d",&curCurve.x,&curCurve.y, &curCurve.d, &curCurve.gene);
-        gameStart(curCurve);
-        initVector(dragonCurve);
-    }
+int main() {
+	scanf("%d", &N);
 
-    int ans = 0;
-    for(int y = 0; y < 99; y++){
-        for(int x = 0 ; x < 99; x ++){
-            if(map[y][x] & map[y][x+1] & map[y+1][x] & map[y+1][x+1]){
-                ans++;
-            }
-        }
-    }
+	for (int n = 1; n <= N; n++) {
+		Curve curCurve = {0,};
+		scanf("%d %d %d %d", &curCurve.x, &curCurve.y, &curCurve.d, &curCurve.gene);
+		curCurve.y += 50;
+		curCurve.x += 50;
+		curCurve.gene += 1;
+		gameStart(curCurve);
+		initVector(dragonCurve);
+	}
 
-    printf("%d ",ans);
+	int ans = 0;
+	for (int y = 0; y < 100; y++) {
+		for (int x = 0; x < 100; x++) {
+			printf("%d ", map[y][x]);
+			if ((map[y][x] != 0)&& (map[y][x + 1]!=0)&&(map[y + 1][x]!=0)&&(map[y + 1][x + 1]!=0)) {
+				ans++;
+			}
+		}
+		printf("\n");
+	}
 
-    return 0;
+	printf("%d ", ans);
+
+	return 0;
 }
