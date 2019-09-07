cb2ea587aac409ae2c3d3f95e0cc38263afb1115
hobbang2 <cattie@pukyong.ac.kr>
2019-09-07 12:57:29 +0900

    solve 17143_낚시왕 s의 경우의 수를 줄이자 ! + 시간복잡도 생각하기

--- 백준알고리즘/17143_낚시왕.cpp
+++ 백준알고리즘/17143_낚시왕.cpp
@@ -14,15 +14,16 @@ int dir_y[5] = { -999,-1,1,0,0 };
 typedef struct Shark {
 	int y, x;
 	int s, d, z;
-	Shark(int _y, int _x, int _s, int _d, int _z) :y(_y), x(_x), s(_s), d(_d), z(_z) {
+	int life;
+	Shark(int _y, int _x, int _s, int _d, int _z, int _life) :y(_y), x(_x), s(_s), d(_d), z(_z), life(_life) {
 	}
 	Shark() {
-		y = x = s = d = z = 0;
+		y = x = s = d = z = life = 0;
 	}
 }Shark;
 
 vector <Shark> sharks;
-vector <vector<Shark> > map(101, vector<Shark>(101, { 0,0,0,0,0 }));
+vector <vector<int> > map(101, vector<int>(101, 0));
 int R = 0, C = 0, M = 0;
 
 void changeDir(Shark & target) {
@@ -43,45 +44,42 @@ void changeDir(Shark & target) {
 }
 
 void moveShark() {
-	vector <vector<Shark> > next_map(101, vector<Shark>(101, { 0,0,0,0,0 }));
+	vector <vector <int> > next_map(101, vector<int>(101, 0));
+	int sharkNum = (int)sharks.size();
 
-	Shark mimic;
-	for (int y = 1; y <= R; y++) {
-		for (int x = 1; x <= C; x++) {
-			if (map[y][x].z != 0) {
-				int curSpeed = map[y][x].s;
-				int next_y = y, next_x = x;
-				if (map[y][x].d <= 2) {
-					curSpeed = curSpeed % ((R - 1) * 2);
-				}
-				else { //좌우로 움직이는 경우
-					curSpeed = curSpeed % ((C - 1) * 2);
-				}
-
-
-				for (int s = 1; s <= curSpeed; s++) {
-					next_y = next_y + dir_y[map[y][x].d];
-					next_x = next_x + dir_x[map[y][x].d];
-					if ((next_y == 0) || (next_y > R)) {
-						changeDir(map[y][x]);
-						next_y = next_y + 2 * dir_y[map[y][x].d];
-
-					}
-					if ((next_x == 0) || (next_x > C)) {
-						changeDir(map[y][x]);
-						next_x = next_x + 2 * dir_x[map[y][x].d];
-					}
-				}
-
-				if (next_map[next_y][next_x].z > map[y][x].z) {
-					map[y][x] = mimic;
-					continue;
-				}
-				map[y][x].y = next_y, map[y][x].x = next_x;
-				next_map[next_y][next_x] = map[y][x];
-				map[y][x] = mimic;
+	for (int idx = 1; idx < sharkNum; idx++) {
+		if (sharks[idx].life == false) {
+			continue;
+		}
+		int curSpeed = sharks[idx].s;
+		int next_y = sharks[idx].y, next_x = sharks[idx].x;
+		if (sharks[idx].d <= 2) {
+			curSpeed = curSpeed % ((R - 1) * 2);
+		}
+		else {
+			curSpeed = curSpeed % ((C - 1) * 2);
+		}
+		for (int s = 1; s <= curSpeed; s++) {
+			next_y = next_y + dir_y[sharks[idx].d];
+			next_x = next_x + dir_x[sharks[idx].d];
+			if ((next_y == 0) || (next_y > R)) {
+				changeDir(sharks[idx]);
+				next_y = next_y + 2 * dir_y[sharks[idx].d];
+			}
+			if ((next_x == 0) || (next_x > C)) {
+				changeDir(sharks[idx]);
+				next_x = next_x + 2 * dir_x[sharks[idx].d];
 			}
 		}
+		int next_idx = next_map[next_y][next_x];
+		if (sharks[next_idx].z > sharks[idx].z) {
+			sharks[idx].life = false;
+			continue;
+		}
+		sharks[next_idx].life = false;
+		next_map[next_y][next_x] = idx;
+		sharks[idx].y = next_y;
+		sharks[idx].x = next_x;
 	}
 	map.swap(next_map);
 }
@@ -89,6 +87,7 @@ void moveShark() {
 int main() {
 	int result = 0;
 	Shark mimicShark, curShark;
+	sharks.push_back({ -1,-1,-1,-1,-1,false });
 
 	scanf("%d %d %d", &R, &C, &M);
 
@@ -97,16 +96,17 @@ int main() {
 		for (int m = 1; m <= M; m++) {
 			int y = 0, x = 0, s = 0, d = 0, z = 0;
 			scanf("%d %d %d %d %d", &y, &x, &s, &d, &z);
-			curShark = { y,x,s,d,z };
-			map[y][x] = curShark;
+			curShark = { y,x,s,d,z,true };
+			sharks.push_back(curShark);
+			map[y][x] = m;
 		}
 
 		// 물고기를 잡아보자. 
 		for (int x = 1; x <= C; x++) {
 			for (int y = 1; y <= R; y++) {
-				if (map[y][x].z != 0) {
-					result += map[y][x].z;
-					map[y][x] = mimicShark;
+				if (map[y][x] != 0) {
+					result += sharks[map[y][x]].z;
+					sharks[map[y][x]].life = false;
 					break;
 				}
 			}
