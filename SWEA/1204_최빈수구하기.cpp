ce05e87b0f2efd9770d9f33ac82f1be64455d23e
hobbang2 <cattie@pukyong.ac.kr>
2019-10-09 16:16:38 +0900

    SOLVE_pair_swea1204_최빈수.cpp

--- SWEA/1204_최빈수구하기.cpp
+++ SWEA/1204_최빈수구하기.cpp
@@ -1,52 +1,35 @@
-// swea 최빈수출력 1204
 #include <cstdio>
 #include <vector>
 #include <utility>
 #include <queue>
 
 #define pii pair<int,int>
-
 using namespace std;
 
-void cleanVec(vector <int> & target) {
-	vector <int> emp(101, 0);
-	target.swap(emp);
-	return;
-}
+vector <pii> score(101,pii(0,0));
 
-void getScore(vector <int> & target) {
-	int curScore = 0;
-	for (int idx = 1; idx <= 1000; idx++) {
-		scanf("%d", &curScore);
-		target[curScore] += 1;
-	}
-	return;
-}
-
-void intoPq(vector <int> & s,priority_queue <pii, vector <pii> > & pq) {
-	pii curPair(0, 0);
-	int idx = 0;
-	for (idx = 1; idx <=100; idx++) {
-		curPair = make_pair(s[idx], idx);
-		pq.push(curPair);
-	}
-	return;
+void varClean() {
+    vector <pii> empty(101,pii(0,0));
+    score.swap(empty);
 }
 
 int main() {
 	int T = 0;
 	int tc = 0;
 	int answer = 0;
-	vector <int> score(101, 0);
-    scanf("%d",&T);
+	int idx = 0;
+	int curScore = 0;
+	scanf("%d", &T);
 	while (T--) {
-		priority_queue <pii, vector <pii> > pq;
-		answer = 0;
 		scanf("%d", &tc);
-		getScore(score);
-		intoPq(score,pq);
+		for (idx = 1; idx <= 1000; idx++) {
+			scanf("%d", &curScore);
+			score[curScore].second = curScore;
+			score[curScore].first += 1;
+		}
+		priority_queue <pii> pq(score.begin()+1, score.end());
 		printf("#%d %d\n", tc, pq.top().second);
-		cleanVec(score);
+        varClean();
 	}
 	return 0;
 }
