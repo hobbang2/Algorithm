28a6d9a90558e3d64b0a763b060a6c2f5176dac5
hobbang2 <cattie@pukyong.ac.kr>
2019-11-02 21:50:48 +0900

    SOLVE N과M(2).cpp

---
+++ 알고리즘풀이/단계별풀기/백트래킹/15650_N과M(2).cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int N, M;
+bool check[10] = { false ,};
+void takeMofN(int idx, int cnt) {
+	if (idx > N+1 || cnt > M) {
+		return;
+	}
+	if (cnt == M) {
+		for (int n = 1; n <= N; n++) {
+			if (check[n] == true) {
+				printf("%d ", n);
+			}
+		}
+		printf("\n");
+		return;
+	}
+	for (int curIdx = idx; curIdx <=N; curIdx++) {
+		check[curIdx] = true;
+		takeMofN(curIdx+1, cnt + 1);
+		check[curIdx] = false;
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	takeMofN(1, 0);
+	return 0;
+}
