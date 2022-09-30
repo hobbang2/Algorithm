f1486ceaa60a031f702a670a7a9508bd4f704291
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2022-09-30 10:44:47 +0900

    [Silver V] Title: 숫자 카드, Time: 1144 ms, Memory: 113256 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,21 +1,15 @@
+from bisect import bisect
+
 # 1부터 50만 , 상근이가 가지고 있는 카드 개수
 N = int(input())
 my_card_list = sorted(list(map(int,input().split())))
 M = int(input())
 check_list = list(map(int,input().split()))
 
-def user_binary_search(low:int, upper:int, target:int, my_cards:list):
-    while( low <= upper):
-        mid = (low + upper)//2
-        cur_num = my_cards[mid]
-
-        if( cur_num > target ):
-            upper = mid - 1 
-        elif( cur_num < target):
-            low = mid + 1
-        else:
-            return int(True)
-    return int(False)
-
 for check_item in check_list:
-    print( user_binary_search(0,N - 1,check_item,my_card_list),end=" ")
+    finded_idx = bisect(my_card_list, check_item) 
+    cur_ans = int(False)
+
+    if( check_item == my_card_list[finded_idx -1] ):
+        cur_ans = int(True)
+    print( cur_ans, end=" ")
