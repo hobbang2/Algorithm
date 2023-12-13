3648a94c1e69979f6a57db8d5646ec9a0c6ebf97
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2023-12-13 09:45:23 +0900

    [level 3] Title: 입국심사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -2,8 +2,7 @@ def solution(n, times):
     # 기다리는 사람 최대 10억명
     # 심사시간 최대 10억분
     # 심사관 10만명 이하
-    times.sort()
-    max_time = (times[-1] * n)
+    max_time = (min(times) * n)
     min_time = 1
     answer = max_time + 1
     
@@ -13,13 +12,15 @@ def solution(n, times):
         
         for t in times:
             process_cnt += (mid_time // t)
+            if(process_cnt >= n):
+                break
             
-        # 처리한 사람이 n명보다 많을 때
+        # 처리한 사람이 n명보다 많을 때 또는 n명 일때
         if(process_cnt >= n):
             # 범위 좁혀서 다시 탐색
             answer = mid_time
             max_time = mid_time - 1
-        # 처리한 사람이 n명보다 작을 때 또는 n명 일때
+        # 처리한 사람이 n명보다 작을 때 
         else:
             # 대기시간을 늘려서 다시 탐색
             min_time = mid_time + 1
