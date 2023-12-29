11d9704d9cbeca1f6b8322ada4d79d076f4c939d
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2023-12-29 12:25:46 +0900

    [level 4] Title: 도둑질, Time: 618.68 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/README.md
+++ 프로그래머스/4/42897. 도둑질/README.md
@@ -1,10 +1,10 @@
 # [level 4] 도둑질 - 42897 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897?language=python3#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42897) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 602.70 ms
+메모리: 95.2 MB, 시간: 618.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 11:9:35
+2023년 12월 5일 12:25:45
 
 ### 문제 설명
 
--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,18 +1,19 @@
 def solution(money):
-    dp_first = [0] * len(money)
-    dp_first[0] = dp_first[1] = money[0]
     
-    for idx in range(2, len(money) - 1):
+    money = [0] + money
+    dp_first = [0] * len(money) 
+    dp_first[1] = dp_first[2] = money[1]
+    
+    # 1번 집 부터 훔쳤으므로, 마지막 집은 방문할 수 없음
+    for idx in range(3, len(money) - 1):
         dp_first[idx] = max(dp_first[idx - 1], money[idx] + dp_first[idx - 2])
 
     dp_second = [0] * len(money)
-    dp_second[0] = 0
-    dp_second[1] = money[1]
+    dp_second[1] = 0 #  1번 집의 금화를 훔치지 않음 
+    dp_second[2] = money[2]
     
     for idx in range(2, len(money)):
         dp_second[idx] = max(dp_second[idx - 1], money[idx] + dp_second[idx - 2])
-
-
     
     answer = max(max(dp_first), max(dp_second))
     return answer
