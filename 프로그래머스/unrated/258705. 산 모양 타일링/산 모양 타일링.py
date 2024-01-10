d92364d1eef7f46019360300e6eb00bba912e06d
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2024-01-10 20:36:37 +0900

    [level unrated] Title: 산 모양 타일링, Time: 50.43 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/unrated/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,14 +1,16 @@
 MOD = 10007
 
-
 def solution(n, tops):
     right_dir_cover = [0 for _ in range(n + 1)]
     left_dir_cover  = [0 for _ in range(n + 1)]
     
-    right_dir_cover[0] = 0
-    left_dir_cover[0]  = 1
+    # right_dir_cover[0] = 0
+    # left_dir_cover[0]  = 1
     
-    for idx, t in enumerate(tops, start = 1):
+    right_dir_cover[1] = 1
+    left_dir_cover[1]  = 3 if 1 == tops[0] else 2
+    
+    for idx, t in enumerate(tops[1:], start = 2):
         
         if( 1 == t):
             left_dir_cover[idx] = ( 2 * right_dir_cover[idx - 1] +\
