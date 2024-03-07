977e210f5ca7a43514beda33ced96d93a3978ae0
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2024-03-07 21:05:38 +0900

    [Gold V] Title: 트리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -5,14 +5,6 @@ N = int(input())
 parents = list(map(int, input().split(' ')))
 remove_node = int(input())
 
-def remove_parents_chain(target_parent:int):
-    global parents
-    if(target_parent < 0 or parents[target_parent] < 0):
-        return 
-    next_remove = parents[target_parent]
-    parents[target_parent] = -2
-    remove_parents_chain(next_remove)
-
 def remove_parents(target_parent:int):
     global parents
 
@@ -30,16 +22,9 @@ def remove_parents(target_parent:int):
 remove_parents(remove_node)
 
 # 어떤 노드의 부모 노드는 리프 노드 일 수 없음
-for p in parents:
-    if(p < 0):
-        continue
-    remove_parents_chain(p)
-    # parents[p] = -2
-
-leaf_node = [p for p in parents if p >= 0]
-result = len(leaf_node)
-
-if(result == 0 and -1 in parents):
-    result = 1
+result = 0 
+for idx in range(N):
+    if((parents[idx] > -2) and (idx not in parents)):
+        result += 1
 
 print(result)
