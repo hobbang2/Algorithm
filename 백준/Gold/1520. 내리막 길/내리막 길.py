dcbd840fb0b7942faa3c17f4ac98224164560499
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2024-04-24 18:21:46 +0900

    [Gold III] Title: 내리막 길, Time: 164 ms, Memory: 44808 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -8,7 +8,6 @@ dr, dc =  [ -1, 1, 0, 0], [0, 0, -1, 1]
 M, N = map(int, input().split(' '))
 user_map = [list(map(int, input().split(' ')))for _ in range(M)]
 info_map = [[0] * N for _ in range(M)]
-visited = [[0] * N for _ in range(M)]
 dp = [[-1] * N for _ in range(M)]
 result = -1
 
@@ -21,7 +20,6 @@ def dfs(r:int , c:int)->int:
     if(dp[r][c] >= 0):
         return dp[r][c]
     
-    available_check = False
     cnt = 0 
     for ddr, ddc in zip(dr, dc):
         next_r, next_c = r + ddr, c + ddc
@@ -29,14 +27,11 @@ def dfs(r:int , c:int)->int:
             continue
         if(0 > next_c or N <= next_c):
             continue
-        if(visited[next_r][next_c] != 0):
-            continue
         if(user_map[next_r][next_c] >= user_map[r][c]):
             continue
         
-        visited[next_r][next_c] = 1
         cnt += dfs(next_r, next_c)
-        visited[next_r][next_c] = 0
+
     dp[r][c] = cnt        
 
     return dp[r][c]
