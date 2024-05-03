d3b7b29ef8a5b8329fc17577ef4aa2958b6b07af
hobbang2 <46401358+hobbang2@users.noreply.github.com>
2024-05-03 13:52:00 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(ingredient):
+    target_ingredient = [1,2,3,1]
+    prepare_hamburger = deque([])
+    tmp_hamburger = deque([])
+    target_idx = 0
+    answer = 0
+    
+    for item in ingredient:
+        
+        if( (1 == item) and ( 3 <= len(prepare_hamburger))):
+            flag = False
+            check_item = []
+            check_item.append(item)
+            check_item.append(prepare_hamburger.pop())
+            check_item.append(prepare_hamburger.pop())
+            check_item.append(prepare_hamburger.pop())
+            if(check_item[0] == 1 and check_item[1] == 3 and check_item[2] == 2 and check_item[3] == 1):
+                answer += 1
+            else:
+                for c in check_item[::-1]:
+                    # if(c != 1):
+                        # break
+                    # else:
+                    prepare_hamburger.append(c)
+                        
+        else:
+            prepare_hamburger.append(item)
+        
+    return answer
